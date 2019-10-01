2eb3a425a12a9b6e85e64da638b4befc22c4b7c2
Steven Scott <stevescott2653@gmail.com>
2019-10-01 16:25:22 -0400

    Add files via upload

---
+++ Method_Overloading/Method_Overloading/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading
+{
+    public class Cal
+    {
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public class TestMemberOverloading
+        {
+            public static void Main()
+            {
+                Console.WriteLine(Cal.add(12, 23));
+                Console.WriteLine(Cal.add(12, 23, 25));
+            }
+        }
+    }
+}
